22bd370b1b000b41d4f611c419c4fe3b71cd4375
Dhruvibhudiya <dhruvibhudiya07@gmail.com>
2026-08-04 22:34:04 +0530

    Time: 11 ms (22.81%), Space: 12.3 MB (51.38%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findMissingElements(self, nums):
+        missing=[]
+        for i in range(min(nums),max(nums)+1):
+            if i not in nums:
+                missing.append(i)
+        return missing
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
